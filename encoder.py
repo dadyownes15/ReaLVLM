d5d49c48a3280d6a42b86569518510c5ee50bd94
Mikkel Janus Dahl <mikkeljanusdahl@gmail.com>
2025-05-14 16:00:59 +0200

    sync

--- encoder.py
+++ encoder.py
@@ -1,18 +1,7 @@
 from src.models.embedding_task import EmbeddingTask
 from pathlib import Path
 
-
-def main():
-    script_dir = Path(__file__).resolve().parent # Gets directory of encoder.py
-    project_root = script_dir.parent # Assumes encoder.py is in project_root
-
-    print(project_root)
-    # If encoder.py is in a 'scripts' subdir, then project_root = script_dir.parent
-    # Adjust project_root definition based on where encoder.py lives relative to 'src'
-
-
-    
-    # Embed using hiera_huge_16x224.mae_k400_ft_k400.json
+"""
     embedding_task2 = EmbeddingTask(
         model_config_path="D:\ReaLVLM\config\embeddings\hiera_huge_16x224.mae_k400_ft_k400.json",
         dataset_name="shanghai_campus",
@@ -21,6 +10,22 @@ def main():
     )
 
     embedding_task2.run()
+"""
+
+def main():
+    script_dir = Path(__file__).resolve().parent # Gets directory of encoder.py
+    project_root = script_dir.parent # Assumes encoder.py is in project_root
+
+    print(project_root)
+
+
+    kiera_base_embedding_Task = EmbeddingTask(
+        model_config_path = "D:\ReaLVLM\config\embeddings\hiera_base_16x224.mae_k400_ft_k400.json",
+        dataset_name="shanghai_campus",
+        training_data_path="D:/ReaLVLM/data/shanghai_campus/training/videos",
+        test_data_path="D:/ReaLVLM/data/shanghai_campus/testing/videos",
+    )
+
     
 
 if __name__ == "__main__":
